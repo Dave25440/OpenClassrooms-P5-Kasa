d9cc4a9240dc62728d1a3b5ad4a2db08951ec5b0
David Horès <hores.d@gmail.com>
2024-07-18 18:28:23 +0200

    Create Layout component with children prop, add Header and Footer components to it and add it to AppRouter

--- src/components/AppRouter/index.jsx
+++ src/components/AppRouter/index.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "../Layout";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
 import Housing from "../../pages/Housing";
@@ -7,12 +8,14 @@ import Error from "../Error";
 const AppRouter = () => {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/a-propos" element={<About />} />
-                <Route path="/logement/:id" element={<Housing />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/a-propos" element={<About />} />
+                    <Route path="/logement/:id" element={<Housing />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </Layout>
         </Router>
     );
 };
