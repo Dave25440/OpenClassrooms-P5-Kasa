18220e3d4ddce39ca5f91cdd9229e512dbd05615
David Horès <hores.d@gmail.com>
2024-07-19 15:35:50 +0200

    Create SCSS Module stylesheet for Footer component, add it to Footer with new Kasa white logo, add color variables and mixin to Layout global stylesheet and use them in Header and Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,17 @@
+import styles from "./Footer.module.scss";
+import WhiteLogo from "../../assets/kasa-white-logo.png";
+
 const Footer = () => {
-    return <footer>Kasa</footer>;
+    return (
+        <footer className={styles.footerblock}>
+            <img
+                src={WhiteLogo}
+                alt="Kasa"
+                className={styles.footerblock__logo}
+            />
+            <p>© 2024 Kasa. All rights reserved</p>
+        </footer>
+    );
 };
 
 export default Footer;
