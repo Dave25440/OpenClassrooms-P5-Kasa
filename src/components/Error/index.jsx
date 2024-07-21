928a94f0d14080e8bac8b224d456effc6ca0475f
David Horès <hores.d@gmail.com>
2024-07-21 16:40:13 +0200

    Create SCSS Module stylesheet for Error component, add it to Error with Link element and update style from Header and Footer stylesheets

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Error.module.scss";
 
 const Error = () => {
     useEffect(() => {
@@ -6,9 +8,15 @@ const Error = () => {
     }, []);
 
     return (
-        <div>
-            <h1>404</h1>
-        </div>
+        <section className={styles.errorblock}>
+            <h1 className={styles.errorblock__title}>404</h1>
+            <p className={styles.errorblock__text}>
+                Oups! La page que vous demandez n'existe pas.
+            </p>
+            <Link to="/" className={styles.errorblock__link}>
+                Retourner sur la page d’accueil
+            </Link>
+        </section>
     );
 };
 
