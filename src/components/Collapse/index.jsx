a3acee8adad016dc68a65b1d2de97572fd2fcc90
David Horès <hores.d@gmail.com>
2024-07-23 18:13:15 +0200

    Move button tag from Collapse component into h2 title and update Collapse stylesheet

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -9,21 +9,23 @@ const Collapse = ({ title, content }) => {
     return (
         <>
             <li className={styles.collapsebar}>
-                <h2 className={styles.collapsebar__title}>{title}</h2>
-                <button
-                    className={`${styles.collapsebar__button} ${
-                        isOpen && styles["collapsebar__button--open"]
-                    }`}
-                    onClick={() => setIsOpen(!isOpen)}
-                >
-                    <picture>
-                        <source
-                            srcSet={SmallChevronUp}
-                            media="(max-width: 768px)"
-                        />
-                        <img src={ChevronUp} alt="Bouton d'affichage" />
-                    </picture>
-                </button>
+                <h2 className={styles.collapsebar__title}>
+                    {title}
+                    <button
+                        className={`${styles.collapsebar__button} ${
+                            isOpen && styles["collapsebar__button--open"]
+                        }`}
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <picture>
+                            <source
+                                srcSet={SmallChevronUp}
+                                media="(max-width: 768px)"
+                            />
+                            <img src={ChevronUp} alt="Bouton d'affichage" />
+                        </picture>
+                    </button>
+                </h2>
                 {isOpen && (
                     <p className={styles.collapsebar__text}>{content}</p>
                 )}
