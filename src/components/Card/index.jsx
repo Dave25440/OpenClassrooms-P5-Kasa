2d3308784c2f6689b43485e17cd2686adc06de7e
David Horès <hores.d@gmail.com>
2024-07-24 00:27:26 +0200

    Add open modifier with grid, padding, opacity properties and translate function to new collapsebar__wrapper selector in Collapse stylesheet, use it with useState hook and logical operator to update block className in Collapse component and add all settings to new collapse-animation mixin

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,18 +3,16 @@ import styles from "./Card.module.scss";
 
 const Card = ({ id, title, cover }) => {
     return (
-        <>
-            <Link to={`/logement/${id}`} className={styles.cardblock}>
-                <article>
-                    <img
-                        src={cover}
-                        alt={title}
-                        className={styles.cardblock__cover}
-                    />
-                    <h2 className={styles.cardblock__title}>{title}</h2>
-                </article>
-            </Link>
-        </>
+        <Link to={`/logement/${id}`} className={styles.cardblock}>
+            <article>
+                <img
+                    src={cover}
+                    alt={title}
+                    className={styles.cardblock__cover}
+                />
+                <h2 className={styles.cardblock__title}>{title}</h2>
+            </article>
+        </Link>
     );
 };
 
--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -7,30 +7,32 @@ const Collapse = ({ title, content }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <>
-            <li className={styles.collapsebar}>
-                <h2 className={styles.collapsebar__title}>
-                    {title}
-                    <button
-                        className={`${styles.collapsebar__button} ${
-                            isOpen && styles["collapsebar__button--open"]
-                        }`}
-                        onClick={() => setIsOpen(!isOpen)}
-                    >
-                        <picture>
-                            <source
-                                srcSet={SmallChevronUp}
-                                media="(max-width: 768px)"
-                            />
-                            <img src={ChevronUp} alt="Bouton d'affichage" />
-                        </picture>
-                    </button>
-                </h2>
-                {isOpen && (
-                    <p className={styles.collapsebar__text}>{content}</p>
-                )}
-            </li>
-        </>
+        <li className={styles.collapsebar}>
+            <h2 className={styles.collapsebar__title}>
+                {title}
+                <button
+                    className={`${styles.collapsebar__button} ${
+                        isOpen && styles["collapsebar__button--open"]
+                    }`}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <picture>
+                        <source
+                            srcSet={SmallChevronUp}
+                            media="(max-width: 768px)"
+                        />
+                        <img src={ChevronUp} alt="Bouton d'affichage" />
+                    </picture>
+                </button>
+            </h2>
+            <div
+                className={`${styles.collapsebar__wrapper} ${
+                    isOpen && styles["collapsebar__wrapper--open"]
+                }`}
+            >
+                <p className={styles.collapsebar__text}>{content}</p>
+            </div>
+        </li>
     );
 };
 
